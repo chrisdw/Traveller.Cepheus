52cb5644eb3c5cc3cabaf5a909fe79dc6a0ef45b
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2018-05-24 13:45:13 +0100

    Generate Orbit data

--- Traveller.SystemGeneration/Sattelite.cs
+++ Traveller.SystemGeneration/Sattelite.cs
@@ -95,11 +95,173 @@ namespace org.DownesWard.Traveller.SystemGeneration
             {
                 dieroll = worldSize - Common.d6();
             }
+
+            if (dieroll == 0)
+            {
+                PlanetType = WorldType.RING;
+                // Rings are in orbits 0-2
+                dieroll = Common.d6();
+                if (dieroll == 6)
+                {
+                    OrbitNumber = 3;
+                }
+                else if (dieroll <= 3)
+                {
+                    OrbitNumber = 1;
+                }
+                else
+                {
+                    OrbitNumber = 2;
+                }
+            }
+            else
+            {
+                if ((dieroll >= worldSize) && (worldSize >= 0))
+                {
+                    dieroll = worldSize - 1;
+                }
+                if (dieroll < 1)
+                {
+                    dieroll = 0;
+                }
+                Normal.Size.Value = 0;
+
+                if (Normal.Size.Value == 0)
+                {
+                    PlanetType = WorldType.SMALL;
+                }
+                else
+                {
+                    PlanetType = WorldType.NORMAL;
+                }
+
+                OrbitNumber = SatOrbit(farAllowed);
+            }
         }
 
         public void SetOrbit(int worldSize, WorldType worldType)
         {
+            var farAllowed = false;
 
+            if (worldType == WorldType.SGG || worldType == WorldType.LGG)
+            {
+                farAllowed = true;
+            }
+
+            if (PlanetType == WorldType.RING)
+            {
+                var dieroll = Common.d6();
+                if (dieroll == 6)
+                {
+                    OrbitNumber = 3;
+                }
+                else if (dieroll <= 3)
+                {
+                    OrbitNumber = 1;
+                }
+                else
+                {
+                    OrbitNumber = 2;
+                }
+            }
+            else
+            {
+                OrbitNumber = SatOrbit(farAllowed);
+            }
+        }
+
+        public int SatOrbit(bool farAllowed)
+        {
+            var dieroll = Common.d6() + Common.d6();
+
+            if (dieroll < 8)
+            {
+                switch (Common.d6() + Common.d6())
+                {
+                    case 2:
+                        return (3);
+                    case 3:
+                        return (4);
+                    case 4:
+                        return (5);
+                    case 5:
+                        return (6);
+                    case 6:
+                        return (7);
+                    case 7:
+                        return (8);
+                    case 8:
+                        return (9);
+                    case 9:
+                        return (10);
+                    case 10:
+                        return (11);
+                    case 11:
+                    case 12:
+                    default:
+                        return (12);
+                }
+            }
+            else if (dieroll < 12 || (!farAllowed && dieroll == 12))
+            {
+                switch (Common.d6() + Common.d6())
+                {
+                    case 2:
+                        return (15);
+                    case 3:
+                        return (20);
+                    case 4:
+                        return (25);
+                    case 5:
+                        return (30);
+                    case 6:
+                        return (35);
+                    case 7:
+                        return (40);
+                    case 8:
+                        return (45);
+                    case 9:
+                        return (50);
+                    case 10:
+                        return (55);
+                    case 11:
+                        return (60);
+                    case 12:
+                        return (65);
+                    default:
+                        return (70);
+                }
+            }
+            else
+            {
+                switch (Common.d6() + Common.d6())
+                {
+                    case 2:
+                        return (75);
+                    case 3:
+                        return (100);
+                    case 4:
+                        return (125);
+                    case 5:
+                        return (150);
+                    case 6:
+                        return (175);
+                    case 7:
+                        return (200);
+                    case 8:
+                        return (225);
+                    case 9:
+                        return (250);
+                    case 10:
+                        return (275);
+                    case 11:
+                        return (300);
+                    case 12:
+                        return (325);
+                    default:
+                        return (350);
+                }
+            }
         }
 
         private void GetTempChart(Planet planet, Orbit myOribit, double ComLumAddFromPrim, Star primary, Configuration configuration)
