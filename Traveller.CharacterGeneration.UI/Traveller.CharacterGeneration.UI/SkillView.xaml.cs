35fbcd724b0ff26552a29eed5761249ebc783ddf
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2018-07-25 16:55:24 +0100

    All skills to be deleted if level coun too high

--- Traveller.CharacterGeneration.UI/Traveller.CharacterGeneration.UI/SkillView.xaml.cs
+++ Traveller.CharacterGeneration.UI/Traveller.CharacterGeneration.UI/SkillView.xaml.cs
@@ -26,15 +26,30 @@ namespace org.DownesWard.Traveller.CharacterGeneration.UI
             var sk = mi.CommandParameter as Skill;
             sk.Level--;
             var character = BindingContext as Character;
+            CheckTotal(character);
+        }
+
+        private void CheckTotal(Character character)
+        {
             var total = character.Skills.Values.Sum(a => a.Level);
             if (total <= character.Profile.Int.Value + character.Profile.Edu.Value)
             {
                 OK.IsEnabled = true;
             }
         }
+
         private async void OK_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
+        }
+
+        private void Removed_Clicked(object sender, EventArgs e)
+        {
+            var mi = sender as MenuItem;
+            var sk = mi.CommandParameter as Skill;
+            var character = BindingContext as Character;
+            character.Skills.Remove(sk.Name);
+            CheckTotal(character);
         }
     }
 }
