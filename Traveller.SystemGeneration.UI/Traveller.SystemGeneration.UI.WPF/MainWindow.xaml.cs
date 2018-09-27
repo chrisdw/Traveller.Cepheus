ccab051ae625bc1e7144c9af76c17a25de5e35f1
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2018-09-27 16:50:48 +0100

    More WPF stuff (still does not work)

---
+++ Traveller.SystemGeneration.UI/Traveller.SystemGeneration.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace org.DownesWard.Traveller.SystemGeneration.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new SystemGeneration.App());
+        }
+    }
+}
