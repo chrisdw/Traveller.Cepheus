f5bb2d0f5f7444a463dd5d2564a3ad57bad4cabf
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2018-12-12 16:44:30 +0000

    Neated up skills a little

--- Traveller.CharacterGeneration/Cepheus/SkillLibrary.cs
+++ Traveller.CharacterGeneration/Cepheus/SkillLibrary.cs
@@ -5,7 +5,7 @@
         public static Skill Advocate = new Skill(Resources.Skill_Advocate, Skill.SkillClass.None, 1, Skill.SkillSex.Female);
         public static Skill Athletics = new Skill(Resources.Skill_Athletics, Skill.SkillClass.None, 1);
         public static Skill Linguistics = new Skill(Resources.Skill_Linguistics, Skill.SkillClass.None, 1);
-        public static Skill NaturalWeapons = new Skill("Natural Weapons", Skill.SkillClass.None, 1);
+        public static Skill NaturalWeapons = new Skill(Resources.Skill_NaturalWeapons, Skill.SkillClass.None, 1);
 
         public static Skill Aircraft = new Skill(Resources.Skill_Aircraft, Skill.SkillClass.None, 1)
         {
@@ -17,7 +17,7 @@
             }
         };
 
-        public static Skill VetinaryMedicine = new Skill(Resources.Skill_VetinaryMedicine, Skill.SkillClass.None, 1);
+        public static Skill VetinaryMedicine = new Skill(Resources.Skill_VetinaryMedicine, Skill.SkillClass.None, 1, Skill.SkillSex.Female);
 
         public static Skill Animals = new Skill(Resources.Skill_Animals, Skill.SkillClass.None, 1)
         {
@@ -44,25 +44,25 @@
             Cascade =
             {
                 new Skill(Resources.Skill_Archery, Skill.SkillClass.None, 1),
-                new Skill(Resources.Skill_EnergyPistol, Skill.SkillClass.None, 1),
-                new Skill(Resources.Skill_EnergyRifle, Skill.SkillClass.None, 1),
+                new Skill(Resources.Skill_EnergyPistol, Skill.SkillClass.Military, 1),
+                new Skill(Resources.Skill_EnergyRifle, Skill.SkillClass.Military, 1),
                 new Skill(Resources.Skill_Shotgun, Skill.SkillClass.None, 1),
                 new Skill(Resources.Skill_SlugPistol, Skill.SkillClass.None, 1),
                 new Skill(Resources.Skill_SlugRifle, Skill.SkillClass.None, 1)
             }
         };
 
-        public static Skill HeavyWeapons = new Skill(Resources.Skill_HeavyWeapons, Skill.SkillClass.None, 1);
+        public static Skill HeavyWeapons = new Skill(Resources.Skill_HeavyWeapons, Skill.SkillClass.Military, 1);
 
-        public static Skill Gunnery = new Skill(Resources.Skill_Gunnery, Skill.SkillClass.None, 1)
+        public static Skill Gunnery = new Skill(Resources.Skill_Gunnery, Skill.SkillClass.Military, 1)
         {
             Cascade =
             {
-                new Skill(Resources.Skill_BayWeapons, Skill.SkillClass.None, 1),
+                new Skill(Resources.Skill_BayWeapons, Skill.SkillClass.Military, 1),
                 HeavyWeapons,
-                new Skill(Resources.Skill_Screens, Skill.SkillClass.None, 1),
-                new Skill(Resources.Skill_SpinalMounts, Skill.SkillClass.None, 1),
-                new Skill(Resources.Skill_TurretWeapons, Skill.SkillClass.None, 1)
+                new Skill(Resources.Skill_Screens, Skill.SkillClass.Military, 1),
+                new Skill(Resources.Skill_SpinalMounts, Skill.SkillClass.Military, 1),
+                new Skill(Resources.Skill_TurretWeapons, Skill.SkillClass.Military, 1)
             }
         };
         public static Skill MeleeCombat = new Skill(Resources.Skill_MeleeCombat, Skill.SkillClass.None, 1)
@@ -70,7 +70,7 @@
             Cascade =
             {
                 new Skill(Resources.Skill_BludgeoningWeapons, Skill.SkillClass.None, 1),
-                new Skill(Resources.Skill_NaturalWeapons, Skill.SkillClass.None, 1),
+                NaturalWeapons,
                 new Skill(Resources.Skill_PiercingWeapons, Skill.SkillClass.None, 1),
                 new Skill(Resources.Skill_SlashingWeapons, Skill.SkillClass.None, 1)
             }
