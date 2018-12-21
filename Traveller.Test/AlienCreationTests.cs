da06d97ef5b839458d771f6c072c7af292086e74
Chris Downes-Ward <c.g.downes-ward@bcs.org.uk>
2018-12-21 11:33:08 +0000

    More tests

--- Traveller.Test/AlienCreationTests.cs
+++ Traveller.Test/AlienCreationTests.cs
@@ -27,5 +27,46 @@ namespace Traveller.Test
             alien.Generate(homeworld);
             alien.Write(Console.Out);
         }
+
+        [TestMethod]
+        public void TestLowGrav()
+        {
+            var config = new Configuration
+            {
+                CurrentCampaign = Campaign.CLASSIC
+            };
+
+            var homeworld = new Planet(config);
+            homeworld.Normal.Starport = 'X';
+            homeworld.Normal.Size.Value = 3;
+            homeworld.Normal.Atmosphere.Value = 4;
+            homeworld.Normal.Hydro.Value = 4;
+            homeworld.Normal.TechLevel.Value = 7;
+            homeworld.Temp = 20;
+            var alien = new Alien() { PsionicsAllowed = true };
+            alien.Generate(homeworld);
+            alien.Write(Console.Out);
+        }
+
+
+        [TestMethod]
+        public void TestDesertLowTech()
+        {
+            var config = new Configuration
+            {
+                CurrentCampaign = Campaign.CLASSIC
+            };
+
+            var homeworld = new Planet(config);
+            homeworld.Normal.Starport = 'X';
+            homeworld.Normal.Size.Value = 8;
+            homeworld.Normal.Atmosphere.Value = 4;
+            homeworld.Normal.Hydro.Value = 0;
+            homeworld.Normal.TechLevel.Value = 0;
+            homeworld.Temp = 20;
+            var alien = new Alien() { PsionicsAllowed = true };
+            alien.Generate(homeworld);
+            alien.Write(Console.Out);
+        }
     }
 }
