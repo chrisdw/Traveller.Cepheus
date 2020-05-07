c11209871d42a5e6b20b95039cfd87a8c658f108
Christopher Downes-Ward <c.g.downes-ward@bcs.org.uk>
2020-05-07 19:06:12 +0100

    Fix typo and update to VS 2019

--- Traveller.CharacterGeneration/Properties/Resources.Designer.cs
+++ Traveller.CharacterGeneration/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace org.DownesWard.Traveller.CharacterGeneration.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1609,7 +1609,7 @@ namespace org.DownesWard.Traveller.CharacterGeneration.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Advaned Eduation.
+        ///   Looks up a localized string similar to Advanced Eduation.
         /// </summary>
         public static string Table_AdvancedEducation {
             get {
